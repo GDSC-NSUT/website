c33f9bd769220214176d1b5eb2193683ed47b1ba
Naman Dureja <durejanaman29@gmail.com>
2022-12-18 02:08:58 +0530

    instagram feed

--- src/Pages/Feed/index.jsx
+++ src/Pages/Feed/index.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
 import "./feed.css";
 
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await fetch("https://graph.instagram.com/me/media?access_token=IGQVJYcWZAPenhVUF94MU5zZAzdTaWt0RHEwWXBCcUJzWUJTZA3p4UWt1YVpOLXNoRllFa1hvZA3YzekZArVmZAnNlNlQTFjYjRlZAXVmaDlNTmZAmbnJ0YnFmS2ltN2txOHB2Y1NYaVdwUmd3&fields=caption,id,media_type,media_url,permalink,thumbnail_url,timestamp");
+      const data = await res.json();
+      setPosts(data.data);
+      console.log(data.data);
+    }
+    getPosts();
+  }, []);
   return (
-    <div>Feed</div>
+    <>
+      <section className="feed-section">
+        <div className="title">Our Feed</div>
+        <div className="content">Follow us on <a href="https://instagram.com/gdsc_nsut" rel="noreferrer" target="_blank" >@GDSC_NSUT</a> and stay tuned in to our upcoming events and announcements.</div>
+        <div className="feeds">
+          {
+            posts.map((post) => (
+              <div className="feed">
+                <a href={post.permalink} target="_blank" rel="noreferrer" >
+                  <img src={post.media_type !== "VIDEO" ? post.media_url : post.thumbnail_url} alt={post.caption} />
+                </a>
+              </div>
+            ))
+          }
+        </div>
+      </section>
+    </>
   )
 }
