8f5b67e404c0f225c8e65e3d2cf0ea05e6b2b2e6
elisedare28 <125211920+elisedare28@users.noreply.github.com>
2023-08-26 16:06:53 +0530

    minor change

--- src/Pages/Team/members.js
+++ src/Pages/Team/members.js
@@ -51,7 +51,7 @@ const CoreMembers = [
     {
         
         "id": 73,
-        "memberImage": "/assets/member-images/rishabh.jpeg",
+        "memberImage": "/assets/member-images/core/rishabh.jpeg",
         "memberName": "Rishabh Choudhary",
         "memberAbout": "",
         "memberSocialLinkedinPath": "https://www.linkedin.com/in/rishabhxchoudhary/",
