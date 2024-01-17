704eda42b3541378f63269a2d55c1b2058397302
singhdsp <ajayps04@outlook.com>
2024-01-17 23:52:00 +0530

    Changed to module.css

--- src/Pages/Blogs/index.jsx
+++ src/Pages/Blogs/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import './blog.css'
+import styles from './blog.module.css'
 
 export default function Blogs() {
   return (
-    <div className='section'>
+    <div className={styles.section}>
       <h1>BLOGS</h1>
       <h6>Comming Soon !!!</h6>
     </div>
